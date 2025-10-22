9399d20c506177cc77dcd4e00b4aebef770f657d
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-10-22 15:10:23 +0500

    added changes

--- src/app/components/layoutcomponens/header/headercomponents/desktopheader.js
+++ src/app/components/layoutcomponens/header/headercomponents/desktopheader.js
@@ -1,12 +1,24 @@
-import HeaderLeftSection from "./headerleftsection";
+import HeaderContactButton from "./headercontactbutton";
 import HeaderRightSection from "./headerrightsection";
+import Link from "next/link";
+import Image from "next/image";
 // import HeaderContactButton from "./headercontactbutton";
 
 export default function DesktopHeader({ isScrolled }) {
   return (
     <div className={`header-main-container ${isScrolled ? "scrolled" : ""}`}>
       {/* <HeaderLeftSection /> */}
+      <Link href="/" title="home">
+        <Image
+          src="/images/Lumhera_logo_no_background.png"
+          alt="FGK logo"
+          width={220}
+          height={53}
+          // className="header-logo-img"
+        />
+      </Link>
       <HeaderRightSection />
+      <HeaderContactButton name="WORK WITH US" />
     </div>
   );
 }
--- src/app/components/layoutcomponens/header/headercomponents/headerrightsection.js
+++ src/app/components/layoutcomponens/header/headercomponents/headerrightsection.js
@@ -1,22 +1,9 @@
 import HeaderRightSectionButtonSection from "./headerrightsectionbuttongroup";
-import HeaderContactButton from "./headercontactbutton";
-import Link from "next/link";
-import Image from "next/image";
 
 export default function HeaderRightSection() {
   return (
     <div className="header-right-buttongroupsection">
-      <Link href="/" title="home">
-        <Image
-          src="/images/Lumhera_logo_no_background.png"
-          alt="FGK logo"
-          width={220}
-          height={53}
-          // className="header-logo-img"
-        />
-      </Link>
       <HeaderRightSectionButtonSection />
-      <HeaderContactButton name="WORK WITH US" />
     </div>
   );
 }
