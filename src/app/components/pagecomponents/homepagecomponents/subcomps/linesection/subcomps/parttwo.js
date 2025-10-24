7094251086b5b29d709004b26562b88f2b1b0e2e
aitizaz-ul-haq <atz.softprgmr@gmail.com>
2025-10-24 17:33:20 +0500

    added changes

--- src/app/components/pagecomponents/homepagecomponents/subcomps/linesection/subcomps/parttwo.js
+++ src/app/components/pagecomponents/homepagecomponents/subcomps/linesection/subcomps/parttwo.js
@@ -3,7 +3,7 @@ import PartTwoRightPart from "./microcomps/parttwo/parttworightpart";
 
 export default function PartTwo({ partTwo }) {
   return (
-    <div className="linesection-partone-container">
+    <div className="linesection-partone-container reverseal">
       <PartTwoLeftPart partTwo={partTwo} />
       <PartTwoRightPart partTwo={partTwo} />
     </div>
